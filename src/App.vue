0c3734c75724f6adc786b3bf81ddc3670ef7be02
Kristina <polskaakristina@gmail.com>
2022-12-10 13:04:48 +0300

    Change default text color

--- src/App.vue
+++ src/App.vue
@@ -133,7 +133,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #383838;
   width: 100%;
 
   .home {
