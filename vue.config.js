dcbe2f929d53c585a42d1aa64cbe6b7d0d7cd09c
Kristina <polskaakristina@gmail.com>
2022-12-12 12:37:03 +0300

    Update vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/dist/'
+      : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Task Manager'
+    }
+  }
 })
+
